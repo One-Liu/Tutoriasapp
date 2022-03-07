1e171f162099b76e5a2af73f989a7d31b8b80f3c
Liu <78963862+One-Liu@users.noreply.github.com>
2022-03-06 21:29:35 -0600

    Clases faltantes

---
+++ src/uv/fei/tutorias/domain/PeriodoEscolar.java
@@ -0,0 +1,35 @@
+package uv.fei.tutorias.domain;
+
+// author @liu
+
+public class PeriodoEscolar {
+	private int idPeriodoEscolar;
+	private String fechaInicio;
+	private String fechaTermino;
+
+	// Getters of uv.fei.tutorias.domain.PeriodoEscolar
+	public int getIdPeriodoEscolar(){
+		return idPeriodoEscolar;
+	}
+
+	public String getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public String getFechaTermino() {
+		return fechaTermino;
+	}
+
+	// Setters of uv.fei.tutorias.domain.PeriodoEscolar
+	public void setIdPeriodoEscolar(int idPeriodoEscolar) {
+		this.idPeriodoEscolar = idPeriodoEscolar;
+	}
+
+	public void setFechaInicio(String fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public void setFechaTermino(String fechaTermino) {
+		this.fechaTermino = fechaTermino;
+	}
+}
---
+++ src/uv/fei/tutorias/domain/SesionDeTutoriaAcademica.java
@@ -0,0 +1,44 @@
+package uv.fei.tutorias.domain;
+
+// author @liu
+
+public class SesionDeTutoriaAcademica {
+	private int idSesionDeTutoriaAcademica;
+	private String fecha;
+	private String hora;
+	private int idPeriodoEscolar;
+
+	// Getters of uv.fei.tutorias.domain.SesionDeTutoriaAcademica
+	public int getIdSesionDeTutoriaAcademica() {
+		return idSesionDeTutoriaAcademica;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public int getIdPeriodoEscolar() {
+		return idPeriodoEscolar;
+	}
+
+	// Setters of uv.fei.tutorias.domain.SesionDeTutoriaAcademica
+	public void setIdSesionDeTutoriaAcademica(int idSesionDeTutoriaAcademica) {
+		this.idSesionDeTutoriaAcademica = idSesionDeTutoriaAcademica;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+
+	public void setIdPeriodoEscolar(int idPeriodoEscolar) {
+		this.idPeriodoEscolar = idPeriodoEscolar;
+	}
+}
