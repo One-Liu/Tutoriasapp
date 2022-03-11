e4a9022e5aaddaee8af473847090956825cf8ba0
Liu <78963862+One-Liu@users.noreply.github.com>
2022-03-10 22:38:30 -0600

    Add files via upload

---
+++ Tutoriasapp/src/uv/fei/tutorias/domain/Persona.java
@@ -0,0 +1,87 @@
+package uv.fei.tutorias.domain;
+
+// author @liu
+
+public class Persona {
+    private int idPersona;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String telefono;
+    private String correoInstitucional;
+
+    public Persona() {
+        nombre = "";
+        apellidoPaterno = "";
+        apellidoMaterno = "";
+        telefono = "";
+        correoInstitucional = "";
+    }
+
+    public Persona(int idPersona, String nombre, String apellidoPaterno, String apellidoMaterno, String telefono, String correoInstitucional) {
+        this.idPersona = idPersona;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.telefono = telefono;
+        this.correoInstitucional = correoInstitucional;
+    }
+
+    public Persona(String nombre, String apellidoPaterno, String apellidoMaterno, String telefono, String correoInstitucional) {
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.telefono = telefono;
+        this.correoInstitucional = correoInstitucional;
+    }
+    
+    // Getters of uv.fei.tutorias.domain.Persona
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreoInstitucional() {
+        return correoInstitucional;
+    }
+
+    // Setters of uv.fei.tutorias.domain.Persona
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setCorreoInstitucional(String correoInstitucional) {
+        this.correoInstitucional = correoInstitucional;
+    }
+}
---
+++ Tutoriasapp/src/uv/fei/tutorias/domain/ProgramaEducativo.java
@@ -0,0 +1,26 @@
+package uv.fei.tutorias.domain;
+
+// author @liu
+
+public class ProgramaEducativo {
+    private int idProgramaEducativo;
+    private String nombreProgramaEducativo;
+
+    // Getters of uv.fei.tutorias.domain.ProgramaEducativo
+    public int getIdProgramaEducativo() {
+        return idProgramaEducativo;
+    }
+
+    public String getNombreProgramaEducativo() {
+        return nombreProgramaEducativo;
+    }
+
+    // Setters of uv.fei.tutorias.domain.ProgramaEducativo
+    public void setIdProgramaEducativo(int idProgramaEducativo) {
+        this.idProgramaEducativo = idProgramaEducativo;
+    }
+
+    public void setNombreProgramaEducativo(String nombreProgramaEducativo) {
+        this.nombreProgramaEducativo = nombreProgramaEducativo;
+    }
+}
