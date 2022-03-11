ddceea1d6de55d844abb9ce0e104515c2a3b617e
L3M-ON <100978191+L3M-ON@users.noreply.github.com>
2022-03-11 16:43:15 -0600

    Add files via upload
    Fixed an oopsie

--- src/main/java/uv/fei/tutorias/bussinesslogic/IEstudianteDAO.java
+++ src/main/java/uv/fei/tutorias/bussinesslogic/IEstudianteDAO.java
@@ -6,10 +6,9 @@ import java.util.List;
 
 //author @L3M-ON
 
-    public interface IEstudianteDAO{
+public interface IEstudianteDAO{
         public List<Persona> findEstudianteByName(String searchName);
         public Persona findEstudianteById(int idEstudiante);
         public boolean addEstudiante(Persona estudiante);
         public boolean deleteEstudienteById(int idEstudiante);
-    }
 }
