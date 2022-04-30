fe4bdb3b4499aa24e39a7abb4aae12b8d9611c6c
Liu <78963862+One-Liu@users.noreply.github.com>
2022-04-30 13:12:31 -0500

    Delete src directory

--- src/main/java/tutoriasapp/Tutoriasapp.java
+++
@@ -1,8 +0,0 @@
-package tutoriasapp;
-
-public class Tutoriasapp {
-    public static void main (String[] args) {
-        GUIRegistroDeEstudiante guiRegistroDeEstudiante = new GUIRegistroDeEstudiante();
-        GUIRegistroDePeriodoEscolar guiRegistroDePeriodoEscolar = new GUIRegistroDePeriodoEscolar();
-    }
-}
