f8d5f48536240c700c8dbe47e0f1abbeb9da0c7e
Paulo <paulocesarhero@gmail.com>
2022-05-12 16:39:48 -0500

    Horario de sesion de tutoria probado y hecho

--- src/main/java/uv/fei/tutorias/bussinesslogic/IHorarioDeSesionDeTutoriaDAO.java
+++ src/main/java/uv/fei/tutorias/bussinesslogic/IHorarioDeSesionDeTutoriaDAO.java
@@ -1,12 +1,13 @@
 package uv.fei.tutorias.bussinesslogic;
 
+import uv.fei.tutorias.domain.HorarioDeSesionDeTutoria;
 import uv.fei.tutorias.domain.Persona;
 
 import java.util.List;
 
 public interface IHorarioDeSesionDeTutoriaDAO {
-//    public List<HorarioDeSesionDeTutoriaAcademica>  (String searchName);
 //    public Persona findTutorAcademicoById(int idTutorAcademico);
 //    public boolean addTutorAcademico(Persona tutorAcademico);
-    public boolean deleteTutorAcademicoById(int idTutorAcademico);
+    public boolean deleteHorarioDeSesionDeTutoria(int searchId);
+    public boolean addHorarioDeSesionDeTutoria(HorarioDeSesionDeTutoria horarioDeSesionDeTutoria);
 }
--- src/main/java/uv/fei/tutorias/domain/SesionDeTutoriaAcademica.java
+++ src/main/java/uv/fei/tutorias/domain/SesionDeTutoriaAcademica.java
@@ -3,14 +3,14 @@ package uv.fei.tutorias.domain;
 // author @liu
 
 public class SesionDeTutoriaAcademica {
-    private int idSesionDeTutoriaAcademica;
+    private int id;
     private String fecha;
     private String hora;
     private int idPeriodoEscolar;
 
     // Getters of uv.fei.tutorias.domain.SesionDeTutoriaAcademica
-    public int getIdSesionDeTutoriaAcademica() {
-        return idSesionDeTutoriaAcademica;
+    public int getId() {
+        return id;
     }
 
     public String getFecha() {
@@ -26,8 +26,8 @@ public class SesionDeTutoriaAcademica {
     }
 
     // Setters of uv.fei.tutorias.domain.SesionDeTutoriaAcademica
-    public void setIdSesionDeTutoriaAcademica(int idSesionDeTutoriaAcademica) {
-        this.idSesionDeTutoriaAcademica = idSesionDeTutoriaAcademica;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setFecha(String fecha) {
