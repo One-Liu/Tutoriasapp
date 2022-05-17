8cc4d631bdbb3380f544239391b6e4c6e6146878
Paulo <paulocesarhero@gmail.com>
2022-05-17 17:25:13 -0500

    Interfaces de alfredo bien idexadas al controlador

--- src/main/java/uv/fei/tutorias/main/GUIModificarSolucionProblematicaAcademicaController.java
+++ src/main/java/uv/fei/tutorias/main/GUIModificarSolucionProblematicaAcademicaController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main.resources.uv.fei.tutorias.main;
+package uv.fei.tutorias.main;
 
 import java.net.URL;
 import java.util.ResourceBundle;
