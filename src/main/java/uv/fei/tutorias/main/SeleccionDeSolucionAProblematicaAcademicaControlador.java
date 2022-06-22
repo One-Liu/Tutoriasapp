76ef1c173782f6a8580daca0363636bf6a8a6ce6
liu <lesterhero2002@gmail.com>
2022-06-21 20:09:40 -0500

    Avances seleccion de solucion

--- src/main/java/uv/fei/tutorias/main/SeleccionDeSolucionAProblematicaAcademicaControlador.java
+++ src/main/java/uv/fei/tutorias/main/SeleccionDeSolucionAProblematicaAcademicaControlador.java
@@ -10,30 +10,66 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import uv.fei.tutorias.bussinesslogic.ExperienciaEducativaDAO;
 import uv.fei.tutorias.bussinesslogic.ProblematicaAcademicaDAO;
+import uv.fei.tutorias.bussinesslogic.ProfesorDAO;
+import uv.fei.tutorias.bussinesslogic.SesionDeTutoriaAcademicaDAO;
+import uv.fei.tutorias.domain.ExperienciaEducativa;
 import uv.fei.tutorias.domain.ProblematicaAcademica;
+import uv.fei.tutorias.domain.Profesor;
+import uv.fei.tutorias.domain.SesionDeTutoriaAcademica;
+import uv.fei.tutorias.utilidades.TablaProblematica_FechaReporte_EE_Profesor;
 import uv.fei.tutorias.utilidades.UtilidadVentana;
 
 public class SeleccionDeSolucionAProblematicaAcademicaControlador implements Initializable {
     
     @FXML
-    private TableView<?> tblProblematicasAcademicas;
+    private TableView<TablaProblematica_FechaReporte_EE_Profesor> tblProblematicasAcademicas;
     @FXML
-    private TableColumn<?,String> clmFechaDeReporte;
+    private TableColumn<TablaProblematica_FechaReporte_EE_Profesor,String> colFechaDeReporte;
     @FXML
-    private TableColumn<?,String> clmProblematicaAcademica;
+    private TableColumn<TablaProblematica_FechaReporte_EE_Profesor,String> colProblematicaAcademica;
     @FXML
-    private TableColumn<?,String> clmExperienciaEducativa;
+    private TableColumn<TablaProblematica_FechaReporte_EE_Profesor,String> colExperienciaEducativa;
     @FXML
-    private TableColumn<?,String> clmProfesor;
+    private TableColumn<TablaProblematica_FechaReporte_EE_Profesor,String> colProfesor;
     
-    private ObservableList<ProblematicaAcademica> problematicasAcademicasConSolucion = FXCollections.observableArrayList();
+    private ObservableList<TablaProblematica_FechaReporte_EE_Profesor> problematicasAcademicasConSolucion;
     
     public void cargarDatos() throws SQLException {
         ProblematicaAcademicaDAO problematicaAcademicaDAO = new ProblematicaAcademicaDAO();
-        this.problematicasAcademicasConSolucion.addAll(problematicaAcademicaDAO.obtenerProblematicasAcademicasConSolucion());
         
+        ObservableList<ProblematicaAcademica> problematicasAcademicas = FXCollections.observableArrayList();
+        problematicasAcademicas.addAll(problematicaAcademicaDAO.obtenerProblematicasAcademicasConSolucion());
         
+        SesionDeTutoriaAcademicaDAO sesionDeTutoriaAcademicaDAO = new SesionDeTutoriaAcademicaDAO();
+        ExperienciaEducativaDAO experienciaEducativaDAO = new ExperienciaEducativaDAO();
+        ProfesorDAO profesorDAO = new ProfesorDAO();
+        TablaProblematica_FechaReporte_EE_Profesor visualizacionProblematica;
+        
+        for(ProblematicaAcademica problematicaAcademica : problematicasAcademicas) {
+            visualizacionProblematica = new TablaProblematica_FechaReporte_EE_Profesor();
+            SesionDeTutoriaAcademica sesionDeTutoriaAcademica = sesionDeTutoriaAcademicaDAO.obtenerSesionDeTutoriaAcademicaPorId(problematicaAcademica.getIdSesionDeTutoriaAcademica());
+            ExperienciaEducativa experienciaEducativa = experienciaEducativaDAO.obtenerExperienciaEducativaPorId(problematicaAcademica.getIdExperienciaEducativa());
+            Profesor profesor = profesorDAO.findProfesorById(experienciaEducativa.getIdProfesor());
+            
+            visualizacionProblematica.setProblematicaAcademica(problematicaAcademica);
+            visualizacionProblematica.setSesionDeTutoriaAcademica(sesionDeTutoriaAcademica);
+            visualizacionProblematica.setExperienciaEducativa(experienciaEducativa);
+            visualizacionProblematica.setProfesor(profesor);
+            this.problematicasAcademicasConSolucion.add(visualizacionProblematica);
+        }
+    }
+    
+    public void cargarCamposGUI() {
+        this.colProblematicaAcademica.setCellValueFactory(new PropertyValueFactory("tituloProblematicaAcademica"));
+        this.colFechaDeReporte.setCellValueFactory(new PropertyValueFactory("fechaDeReporte"));
+        this.colExperienciaEducativa.setCellValueFactory(new PropertyValueFactory("nombreEE"));
+        this.colProfesor.setCellValueFactory(new PropertyValueFactory("nombreProfesor"));
+        
+        this.tblProblematicasAcademicas.setItems(this.problematicasAcademicasConSolucion);
+        this.tblProblematicasAcademicas.getSelectionModel().clearSelection();
     }
     
     @Override
